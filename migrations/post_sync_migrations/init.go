0b6d5a2499cfbb833397117b8d995285f728cd4f
superzordon <zordonclout@gmail.com>
2023-07-31 19:40:48 -0400

    Fix refresh

--- migrations/post_sync_migrations/init.go
+++ migrations/post_sync_migrations/init.go
@@ -15,38 +15,38 @@ var (
 		Query  string
 		Ticker *time.Ticker
 	}{
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_count_all", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_count_30_d", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_wallet_count_all", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_new_wallet_count_30_d", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_active_wallet_count_30_d", Ticker: time.NewTicker(15 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_block_height_current", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_count_pending", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_fee_1_d", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_total_supply", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_post_count", Ticker: time.NewTicker(30 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_comment_count", Ticker: time.NewTicker(30 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_repost_count", Ticker: time.NewTicker(30 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_count_creator_coin", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_count_nft", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_count_dex", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_count_social", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_follow_count", Ticker: time.NewTicker(60 * time.Second)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_message_count", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_count_all", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_count_30_d", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_wallet_count_all", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_new_wallet_count_30_d", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_active_wallet_count_30_d", Ticker: time.NewTicker(15 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_block_height_current", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_count_pending", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_fee_1_d", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_total_supply", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_post_count", Ticker: time.NewTicker(30 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_comment_count", Ticker: time.NewTicker(30 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_repost_count", Ticker: time.NewTicker(30 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_count_creator_coin", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_count_nft", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_count_dex", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_count_social", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_follow_count", Ticker: time.NewTicker(60 * time.Second)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_message_count", Ticker: time.NewTicker(60 * time.Second)},
 		{Query: "SELECT refresh_public_key_first_transaction()", Ticker: time.NewTicker(5 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_social_leaderboard_likes", Ticker: time.NewTicker(10 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_social_leaderboard_reactions", Ticker: time.NewTicker(15 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_social_leaderboard_diamonds", Ticker: time.NewTicker(15 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_social_leaderboard_reposts", Ticker: time.NewTicker(15 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_social_leaderboard_comments", Ticker: time.NewTicker(15 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_social_leaderboard", Ticker: time.NewTicker(15 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_nft_leaderboard", Ticker: time.NewTicker(15 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_defi_leaderboard", Ticker: time.NewTicker(15 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_count_monthly", Ticker: time.NewTicker(10 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_wallet_count_monthly", Ticker: time.NewTicker(10 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_txn_count_daily", Ticker: time.NewTicker(10 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_new_wallet_count_daily", Ticker: time.NewTicker(10 * time.Minute)},
-		{Query: "REFRESH MATERIALIZED VIEW statistic_active_wallet_count_daily", Ticker: time.NewTicker(10 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_social_leaderboard_likes", Ticker: time.NewTicker(10 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_social_leaderboard_reactions", Ticker: time.NewTicker(15 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_social_leaderboard_diamonds", Ticker: time.NewTicker(15 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_social_leaderboard_reposts", Ticker: time.NewTicker(15 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_social_leaderboard_comments", Ticker: time.NewTicker(15 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_social_leaderboard", Ticker: time.NewTicker(15 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_nft_leaderboard", Ticker: time.NewTicker(15 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_defi_leaderboard", Ticker: time.NewTicker(15 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_count_monthly", Ticker: time.NewTicker(10 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_wallet_count_monthly", Ticker: time.NewTicker(10 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_txn_count_daily", Ticker: time.NewTicker(10 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_new_wallet_count_daily", Ticker: time.NewTicker(10 * time.Minute)},
+		{Query: "REFRESH MATERIALIZED VIEW CONCURRENTLY statistic_active_wallet_count_daily", Ticker: time.NewTicker(10 * time.Minute)},
 	}
 )
 
@@ -55,6 +55,8 @@ func SetCalculateExplorerStatistics(calculate bool) {
 }
 
 func RefreshExplorerStatistics(db *bun.DB) {
+	// Stop this for now so Stas can still work w/ the db until I fix the concurrent refresh issue
+	return
 	// Only run if explorer statistics are enabled.
 	if !calculateExplorerStatistics {
 		return
