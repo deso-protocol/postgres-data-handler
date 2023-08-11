861dd782b24dbbf164bf9de05d0665f98be9b0b8
Jackson Dean <jckson.dean@gmail.com>
2023-08-11 12:17:54 -0600

    fix cc total value calculation

--- migrations/post_sync_migrations/20230709000000_create_creator_coin_view.go
+++ migrations/post_sync_migrations/20230709000000_create_creator_coin_view.go
@@ -16,7 +16,7 @@ func init() {
 				balance_entry.balance_nanos,
 				balance_entry.has_purchased,
     		profile_entry.coin_price_deso_nanos,
-    		(balance_entry.balance_nanos * coin_price_deso_nanos)::NUMERIC AS total_value_nanos
+    		(((profile_entry.deso_locked_nanos * (1 - POWER(1 - balance_entry.balance_nanos / profile_entry.cc_coins_in_circulation_nanos, 1 / 0.3333333))) * 9999) / 10000)::NUMERIC AS total_value_nanos
 			FROM balance_entry
 			JOIN profile_entry ON profile_entry.pkid = balance_entry.creator_pkid
 			WHERE is_dao_coin = false;
