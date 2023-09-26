499f69230044018b66b0d8fd830eaa27d1355f63
superzordon <zordonclout@gmail.com>
2023-09-26 16:46:27 -0400

    Update db connect logic

--- main.go
+++ main.go
@@ -17,6 +17,11 @@ import (
 	"github.com/uptrace/bun/extra/bundebug"
 	"gopkg.in/DataDog/dd-trace-go.v1/ddtrace/tracer"
 	"gopkg.in/DataDog/dd-trace-go.v1/profiler"
+	"time"
+)
+
+const (
+	maxRetries = 10
 )
 
 func main() {
@@ -134,10 +139,20 @@ func getConfigValues() (pgURI string, stateChangeDir string, consumerProgressDir
 }
 
 func setupDb(pgURI string, threadLimit int, logQueries bool, readonlyUserPassword string, calculateExplorerStatistics bool) (*bun.DB, error) {
-	// Open a PostgreSQL database.
-	pgdb := sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(pgURI)))
+	// Attempt to connect to the DB, retrying every 5 seconds until it's up, up to maxRetry times.
+	var pgdb *sql.DB
+	retryCount := 0
+	for retryCount < maxRetries {
+		pgdb = sql.OpenDB(pgdriver.NewConnector(pgdriver.WithDSN(pgURI)))
+		if pgdb == nil {
+			retryCount++
+			glog.Infof("Error connecting to postgres db at URI: %v. Retrying in 5 seconds...", pgURI)
+			time.Sleep(5 * time.Second)
+		}
+	}
+
 	if pgdb == nil {
-		glog.Fatalf("Error connecting to postgres db at URI: %v", pgURI)
+		glog.Fatalf("Error connecting to postgres db at URI: %v after %v retries", pgURI, maxRetries)
 	}
 
 	// Create a Bun db on top of postgres for querying.
