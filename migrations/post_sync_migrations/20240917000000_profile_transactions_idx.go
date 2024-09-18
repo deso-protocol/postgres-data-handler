baa8e94e36d3588a67ac4023d5fe76ea4cbdb6e2
superzordon <zordonclout@gmail.com>
2024-09-17 21:33:24 -0400

    Added index

---
+++ migrations/post_sync_migrations/20240917000000_profile_transactions_idx.go
@@ -0,0 +1,35 @@
+package post_sync_migrations
+
+import (
+	"context"
+
+	"github.com/uptrace/bun"
+)
+
+func init() {
+	Migrations.MustRegister(func(ctx context.Context, db *bun.DB) error {
+		if !calculateExplorerStatistics {
+			return nil
+		}
+		_, err := db.Exec(`
+		create index if not exists statistic_profile_transactions_latest_idx on statistic_profile_transactions (latest_transaction_timestamp desc);
+`)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, func(ctx context.Context, db *bun.DB) error {
+		if !calculateExplorerStatistics {
+			return nil
+		}
+		_, err := db.Exec(`
+		drop index if exists statistic_profile_transactions_latest_idx;
+		`)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
